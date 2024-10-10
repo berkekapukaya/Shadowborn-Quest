24363b4574314ebb9462b771c1274ba92f4e99bb
berkekapukaya <73217005+berkekapukaya@users.noreply.github.com>
2024-10-10 17:12:07 +0300

    feat: Player Controlls and Basic Animations

---
+++ Assets/Scripts/Helpers/StringConstants.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Helpers
+{
+    public static class StringConstants
+    {
+        public const string moveX = "moveX";
+        public const string moveY = "moveY";
+    }
+}
